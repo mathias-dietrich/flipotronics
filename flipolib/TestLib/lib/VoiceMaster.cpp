145601860ce486246f9759d62fcae7ab6fd6af8e
mathias-dietrich <mdietric@redhat.com>
2021-03-02 10:43:53 +0100

    interface added

---
+++ flipolib/TestLib/lib/VoiceMaster.cpp
@@ -0,0 +1,8 @@
+//
+//  VoiceMaster.cpp
+//  TestLib
+//
+//  Created by Mathias Dietrich on 02.03.21.
+//
+
+#include "VoiceMaster.hpp"
---
+++ flipolib/TestLib/lib/VoiceMaster.hpp
@@ -0,0 +1,13 @@
+//
+//  VoiceMaster.hpp
+//  TestLib
+//
+//  Created by Mathias Dietrich on 02.03.21.
+//
+
+#ifndef VoiceMaster_hpp
+#define VoiceMaster_hpp
+
+#include <stdio.h>
+
+#endif /* VoiceMaster_hpp */
