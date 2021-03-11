0264997491bfae063c2266f60419ae4bf4769fd2
mathias-dietrich <mdietric@redhat.com>
2021-03-11 14:40:37 +0100

    updaes

--- PatchBuilder/PatchBuilder/Midi.swift
+++ PatchBuilder/PatchBuilder/Midi.swift
@@ -16,31 +16,66 @@ class Midi {
     // establish a MIDI client and output port, and send a note on/off pair.
     var midiClient:MIDIClientRef = 0
     var outPort:MIDIPortRef = 0
+    var inPort:MIDIPortRef = 0
     var dest:MIDIEndpointRef = 0
     var destName = ""
+    
+    var midiClientListen = MIDIClientRef()
+    
+    func myNotifyCallback(message:UnsafePointer<MIDINotification>) -> Void {
+            print("callback")
+    }
     
     func setup(){
         destinationNames = midiDestinationNames()
         
         MIDIClientCreate("Swift3 Test Client" as CFString, nil, nil, &midiClient)
         MIDIOutputPortCreate(midiClient, "Swift3 Test OutPort" as CFString, &outPort)
+        
+    
+       // let clientName = "com.flipotronics.patch" as CFString
+        let clientName = destinationNames[0] as CFString
+        let err = MIDIClientCreateWithBlock(clientName, &midiClientListen) { (notificationPtr: UnsafePointer<MIDINotification>) in
+            let notification = notificationPtr.pointee
+            switch notification.messageID {
+                case .msgSetupChanged: // Can ignore, really
+                    break
 
-        let destNum = 0
-       destName = destinationNames[destNum]
+                case .msgObjectAdded:
+                    let rawPtr = UnsafeRawPointer(notificationPtr)
+                    let message = rawPtr.assumingMemoryBound(to: MIDIObjectAddRemoveNotification.self).pointee
+                    print("MIDI \(message.childType) added: \(message.child)")
+
+                case .msgObjectRemoved:
+                    let rawPtr = UnsafeRawPointer(notificationPtr)
+                    let message = rawPtr.assumingMemoryBound(to: MIDIObjectAddRemoveNotification.self).pointee
+                    print("MIDI \(message.childType) removed: \(message.child)")
+
+                case .msgPropertyChanged:
+                    let rawPtr = UnsafeRawPointer(notificationPtr)
+                    let message = rawPtr.assumingMemoryBound(to: MIDIObjectPropertyChangeNotification.self).pointee
+                    print("MIDI \(message.object) property \(message.propertyName.takeUnretainedValue()) changed.")
+
+                case .msgThruConnectionsChanged:
+                    fallthrough
+                case .msgSerialPortOwnerChanged:
+                    print("MIDI Thru connection was created or destroyed")
+
+                case .msgIOError:
+                    let rawPtr = UnsafeRawPointer(notificationPtr)
+                    let message = rawPtr.assumingMemoryBound(to: MIDIIOErrorNotification.self).pointee
+                    print("MIDI I/O error \(message.errorCode) occurred")
+
+                default:
+                    break
+            }
+        }
+
+        let destNum = 2
+        destName = destinationNames[destNum]
         dest = MIDIGetDestination(destNum)
-
-    }
-    
-    func notifyCallback(message:UnsafePointer<MIDINotification>,refCon:UnsafeMutablePointer<Void>)
-    {
-        print("MIDI Notify")
     }
 
-    func eventCallback(pktlist:UnsafePointer<MIDIPacketList>, refCon:UnsafeMutablePointer<Void>, connRefCon:UnsafeMutablePointer<Void>)
-    {
-        print("MIDI Read")
-    }
-    
     // method to collect display names of available MIDI destinations
     func midiDestinationNames() -> [String] {
         var names:[String] = []
@@ -83,7 +118,7 @@ class Midi {
             midiPacket.data.2 = UInt8(value)
 
             var packetList:MIDIPacketList = MIDIPacketList(numPackets: 1, packet: midiPacket)
-            print("Sending note on to \(destName)")
+            print("Sending midi on to \(destName)")
             MIDISend(outPort, dest, &packetList)
 
             /*
@@ -93,8 +128,6 @@ class Midi {
             packetList = MIDIPacketList(numPackets: 1, packet: midiPacket)
             MIDISend(outPort, dest, &packetList)
              */
-            print("Note off sent to \(destName)")
         }
     }
-
 }
